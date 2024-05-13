d449878c642a3591837dc183701b60fb08759977
yc62897441 <yc62897441@gmail.com>
2024-05-13 11:15:15 +0800

    設定 css-in-js styled-components，避免到了瀏覽器才執行 JS 渲染 CSS 樣式，造成畫面版面異動

---
+++ app/lib/registry.tsx
@@ -0,0 +1,28 @@
+// 設定 css-in-js styled-components
+// 避免到了瀏覽器才執行 JS 渲染 CSS 樣式，造成畫面版面異動
+// https://nextjs.org/docs/app/building-your-application/styling/css-in-js#styled-components
+'use client'
+
+import React, { useState } from 'react'
+import { useServerInsertedHTML } from 'next/navigation'
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+
+export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
+    // Only create stylesheet once with lazy initial state
+    // x-ref: https://reactjs.org/docs/hooks-reference.html#lazy-initial-state
+    const [styledComponentsStyleSheet] = useState(() => new ServerStyleSheet())
+
+    useServerInsertedHTML(() => {
+        const styles = styledComponentsStyleSheet.getStyleElement()
+        styledComponentsStyleSheet.instance.clearTag()
+        return <>{styles}</>
+    })
+
+    if (typeof window !== 'undefined') return <>{children}</>
+
+    return (
+        <StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+            {children}
+        </StyleSheetManager>
+    )
+}
--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com'],
     },
+
+    // 設定 css-in-js styled-components
+    // 避免到了瀏覽器才執行 JS 渲染 CSS 樣式，造成畫面版面異動
+    // https://nextjs.org/docs/app/building-your-application/styling/css-in-js#styled-components
+
+    // Styles take time to load - Next.js
+    // https://stackoverflow.com/questions/74242349/styles-take-time-to-load-next-js
+    // 解決剛載入網頁時，只有 HTML 結構卻沒有 CSS 樣式的問題，須等短暫秒數才會渲染 CSS 的問題。
+    compiler: {
+        styledComponents: true,
+    },
 }
 
 export default nextConfig
